68a5b1e03ffd990c34047d20aedab39f3a33d100
DeGenius360 <degenius8@gmail.com>
2023-09-27 17:18:55 +0100

    start from scratch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import React from 'react';
-import SideNav from './SideNav';
-import Tracker from './components/Tracker';
-import Company from './components/Company';
-import {Routes, Route, BrowserRouter } from 'react-router-dom';
-import Applications from './components/Applications';
-import Home from './components/Home';
-import { createTheme, ThemeProvider } from "@mui/material";
+import React from 'react'
 
-
-
-
-
-function App() {
-  const theme = createTheme({
-    spacing: 1, // Adjust this value to change the spacing
-  });
+const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={< Applications />}></Route> 
-          <Route path="/tracker" exact element={< Tracker />}></Route> 
-          <Route path="/company" exact element={< Company />}></Route>
-          <Route path="/home" exact element={< Home />}></Route> 
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    <div>
+      Hello World!
+    </div>
+  )
 }
 
-export default App;
+export default App
