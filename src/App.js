edcac8fe77831984fd8cf11c7393689bfdc6a242
DeGenius360 <degenius8@gmail.com>
2023-09-30 22:34:03 +0100

    last point for "BarChart"

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import  Dashboard  from "./scenes/dashboard";
- import Team from "./scenes/team"
+import Team from "./scenes/team"
 import Invoice from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import  Calendar  from "./scenes/calendar";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
+import FAQ from "./scenes/faq";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
+
 // import Geography from "./scenes/geography";
 
 
@@ -29,14 +30,14 @@ const App = () => {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-               <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoice />} /> 
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} /> 
               <Route path="/faq" element={<FAQ />} />
-              {/* // <Route path="/bar" element={<Bar />} />
-              // <Route path="/pie" element={<Pie />} />
+              <Route path="/bar" element={<Bar />} />
+              {/*// <Route path="/pie" element={<Pie />} />
               // <Route path="/line" element={<Line />} />
               // <Route path="/geography" element={<Geography />} /> */}
                  
