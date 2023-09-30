bfee2d5a5e05e7af8071be211989cd5314aea665
DeGenius360 <degenius8@gmail.com>
2023-09-30 22:06:23 +0100

    "FAQ" Page added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import  Dashboard  from "./scenes/dashboard";
  import Team from "./scenes/team"
 import Invoice from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
 import Form from "./scenes/form";
+import  Calendar  from "./scenes/calendar";
+// import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import  Calendar  from "./scenes/calendar";
+
 
 
 const App = () => {
@@ -32,12 +33,13 @@ const App = () => {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoice />} /> 
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} /> 
+              <Route path="/faq" element={<FAQ />} />
               {/* // <Route path="/bar" element={<Bar />} />
               // <Route path="/pie" element={<Pie />} />
               // <Route path="/line" element={<Line />} />
-              // <Route path="/faq" element={<FAQ />} />
-              // <Route path="/geography" element={<Geography />} />
-              // <Route path="/calendar" element={<Calendar />} /> */}    
+              // <Route path="/geography" element={<Geography />} /> */}
+                 
             </Routes>
           </main>
         </div>
