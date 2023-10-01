faca94fa87064472984078ed2b5580424b4b37e7
DeGenius360 <degenius8@gmail.com>
2023-10-01 23:23:04 +0100

    last place to end, "Dashboard"

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,14 +1,144 @@
-import { Box } from '@mui/material';
-import Header from '../../components/Header';
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import BarChart from "../../components/BarChart";
+import LineChart from "../../components/LineChart";
+import GeographyChart from "../../components/GeographyChart";
+import StatsBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.colors);
+
   return (
-    <Box m="20px">Dashboard
+    <Box m="20px">
+      Dashboard
       <Box display="flex" justifyContent="space-between" alignItems="center">
-      <Header title="DASHBOARD"  subtitle="Welcome to your dashboard "/>
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard " />
+      </Box>
+      <Box>
+        <Button
+          sx={{
+            backgroundColor: colors.blueAccent[700],
+            color: colors.grey[100],
+            fontSize: "14px",
+            fontWeight: "bold",
+            padding: "10px 20px",
+          }}
+        >
+          <DownloadOutlinedIcon sx={{ marginRight: "10px" }} />
+          Download Reports
+        </Button>
+      </Box>
+      {/* GRID AND CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatsBox
+            title="12,361"
+            subtitle="Emails Sent"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <EmailIcon
+                sx={{
+                  fontSize: "26px",
+                  color: colors.greenAccent[600],
+                }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatsBox
+            title="431,225"
+            subtitle="Sales Obtained"
+            progress="0.5"
+            increase="+21%"
+            icon={
+              <PointOfSaleIcon
+                sx={{
+                  fontSize: "26px",
+                  color: colors.greenAccent[600],
+                }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatsBox
+            title="32,441"
+            subtitle="New Clients"
+            progress="0.35"
+            increase="+5%"
+            icon={
+              <PersonAddIcon
+                sx={{
+                  fontSize: "26px",
+                  color: colors.greenAccent[600],
+                }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatsBox
+            title="1,325,134"
+            subtitle="Traffic Inbound"
+            progress="0.80"
+            increase="+43%"
+            icon={
+              <TrafficIcon
+                sx={{
+                  fontSize: "26px",
+                  color: colors.greenAccent[600],
+                }}
+              />
+            }
+          />
+        </Box>
+
+        {/* ROW 2 */}
+        
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default Dashboard;
